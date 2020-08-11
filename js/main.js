ed778d3fe6e1d9277b2dc212c3a5a0f70a30d8f6
SagarSabat <sagarssabat@gmail.com>
2020-08-11 21:33:09 +0530

    daterange picker custom styling and theming

--- js/main.js
+++ js/main.js
@@ -6,12 +6,28 @@ $(document).ready(function () {
     });
   });
 
+  // $('input[name="daterange"]').daterangepicker({
+  //   opens: 'left'
+  // }, function (start, end, label) {
+  //   console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  // });
   $('input[name="daterange"]').daterangepicker({
-    opens: 'left'
+    "showWeekNumbers": true,
+    ranges: {
+      'Today': [moment(), moment()],
+      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month': [moment().startOf('month'), moment().endOf('month')],
+      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    "alwaysShowCalendars": true,
+    "startDate": "08/05/2020",
+    "endDate": "08/11/2020",
+    "opens": "center"
   }, function (start, end, label) {
-    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
   });
-
 
   $('[data-toggle="tooltip"]').tooltip(show);
 
